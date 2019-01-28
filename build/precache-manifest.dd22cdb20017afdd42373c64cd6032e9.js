90b2035e4cb20f31ee30436dd8906207856d8209
Cody Troop <codytroop@yahoo.com>
2019-01-28 16:09:53 -0600

    added build folder to gitignore

---
+++ build/precache-manifest.dd22cdb20017afdd42373c64cd6032e9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "00dadc495316147ba910",
+    "url": "/RAGSOOK/weekend-challenge-5-feedback/static/css/main.b5ca0d72.chunk.css"
+  },
+  {
+    "revision": "00dadc495316147ba910",
+    "url": "/RAGSOOK/weekend-challenge-5-feedback/static/js/main.00dadc49.chunk.js"
+  },
+  {
+    "revision": "33cac8e4871b9a33a5cc",
+    "url": "/RAGSOOK/weekend-challenge-5-feedback/static/js/1.33cac8e4.chunk.js"
+  },
+  {
+    "revision": "2b97f2368f137ae3ef4a",
+    "url": "/RAGSOOK/weekend-challenge-5-feedback/static/js/runtime~main.2b97f236.js"
+  },
+  {
+    "revision": "9b788223f4a9aabe3ec9f9150d0d58dd",
+    "url": "/RAGSOOK/weekend-challenge-5-feedback/index.html"
+  }
+];
